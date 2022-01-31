0dbe3c663bf6ec2afd74b025b5711c085396dde9
luizsac <lu_que90@hotmail.com>
2022-01-31 16:35:15 -0300

    First commit

---
+++ src/main/java/com/dio/personapi/resource/PersonController.java
@@ -0,0 +1,18 @@
+package com.dio.personapi.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Well, Hello!", HttpStatus.OK);
+    }
+
+}
