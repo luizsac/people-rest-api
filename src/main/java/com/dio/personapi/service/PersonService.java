9fc24955e9d2c6855cde9965418b839f784ed4d3
luizsac <lu_que90@hotmail.com>
2022-02-01 19:52:10 -0300

    Update and delete methods implemented

--- src/main/java/com/dio/personapi/service/PersonService.java
+++ src/main/java/com/dio/personapi/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.dio.personapi.service;
 
 import com.dio.personapi.dto.request.PersonDTO;
+import com.dio.personapi.exception.PersonNotFoundException;
 
 import java.util.List;
 
@@ -8,4 +9,7 @@ public interface PersonService {
 
     PersonDTO create(PersonDTO newPersonDTO);
     List<PersonDTO> getAll();
+    PersonDTO getById(Long id) throws PersonNotFoundException;
+    PersonDTO update(Long id, PersonDTO personDTO) throws PersonNotFoundException;
+    PersonDTO delete(Long id) throws PersonNotFoundException;
 }
